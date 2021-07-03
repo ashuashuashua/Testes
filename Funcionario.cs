dbbb24a9a391b2627252a49b8606eb5f81921774
Mbeevee <mystykys1802@gmail.com>
2021-07-03 11:51:30 -0300

    Iniciado o projeto kkkkk

---
+++ Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Curso
+{
+    class Funcionario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario}";
+        }
+        public void Aumento(double porc)
+        {
+            Salario = ((Salario * porc) / 100)+Salario;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionario> list = new List <Funcionario>();
+            Console.WriteLine("Quantos funcionários irão ser coisados?");
+            int aux = int.Parse(Console.ReadLine());
+            for (int i = 0; i< aux; i++)
+            {
+                Console.WriteLine("id");
+                int lul = int.Parse(Console.ReadLine());
+                Console.WriteLine("nome");
+                string nome = Console.ReadLine();
+                Console.WriteLine("salario");
+                double salario = double.Parse(Console.ReadLine());
+                list.Add(new Funcionario(lul, nome, salario));
+            }
+            Console.WriteLine("passa o id do aumento");
+            int id = int.Parse(Console.ReadLine());
+            Funcionario a = list.Find(x => x.Id == id);
+            Console.WriteLine("passa o aumento");
+            double aum = double.Parse(Console.ReadLine());
+            a.Aumento(aum);
+            foreach(Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            
+        }
+    }
+}
